c501a56873f4f7d9b7005bea4660e2ea97e469e2
sebfro <sebastian17pepp@gmail.com>
2017-05-09 12:27:38 +0200

    Test for linux

--- imports/ui/TakePhoto.jsx
+++ imports/ui/TakePhoto.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import {Meteor, MeteorCamera} from 'meteor/meteor';
 
 export default class TakePhoto extends Component{
 
     takePicture(event){
+        console.log("hei");
         event.preventDefault();
+        let cameraOptions = {
+            height: 600,
+            width: 800,
+            quality: 100
+        };
+        console.log("hei");
+        MeteorCamera.getPicture(cameraOptions, function (error, data){
+            if(!error){
+                ReactDOM.findDOMNode(this.refs.bilde).src = data;
+            }
+        });
     }
 
     render(){
