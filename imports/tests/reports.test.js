f12ace3ad453eaf8a4323758510f0119f15114bd
sebastian17pepp@gmail.com <fhkwxexkp7cbr9cw>
2017-06-02 13:47:07 +0200

    Begynt å skrive tester for bakgrunns oppgaver, små forandringer

--- imports/tests/reports.test.js
+++ imports/tests/reports.test.js
@@ -1,11 +1,35 @@
 /**
  * Created by sebastian on 02.06.17.
  */
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import {Factory} from 'meteor/dburles:factory'
+import React from 'react';
+import {shallow} from 'enzyme';
+import {chai, assert} from 'meteor/practicalmeteor:chai';
+import {Reports} from '../api/reports.js';
 
-describe('reports', function () {
-    it('does something that should be tested', function () {
-        // This code will be executed by the test driver when the app is started
-        // in the correct mode
-    })
-})
+if (Meteor.isServer) {
+    describe('Reports', function () {
+        describe('Methods', function () {
+            const userId = Random.id();
+            let reportId;
+
+            beforeEach(() => {
+                Reports.remove({});
+                reportId = Reports.insert({
+                    text: 'test report',
+                    createdAt: new Date(),
+                    owner: userId,
+                    username: 'seb',
+                });
+            });
+            it('can delte report', function () {
+                const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+                const invocation = { userId };
+                deleteTask.apply(invocation, [reportId]);
+                assert.equal(Reports.find().count(), 0);
+            })
+        })
+    });
+}
